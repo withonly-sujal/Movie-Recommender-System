95b41d34da595579879a9c9dfbf3a5377e5677e9
withonly-sujal <sujal.kadam20805@gmail.com>
2025-07-08 18:59:26 +0530

    Upload

---
+++ recommender/movierecommender/views.py
@@ -0,0 +1,31 @@
+from . import views
+from .models import Movie
+from django.shortcuts import render
+
+
+def movie_recommendation_view(request):
+    if request.method == "GET":
+
+      context = generate_movies_context()
+
+      return render(request, 'movierecommender/movie_list.html', context)
+    
+    
+def generate_movies_context():
+    context = {}
+
+    recommended_count = Movie.objects.filter(
+        recommended=True
+    ).count()
+    if recommended_count == 0:
+        movies = Movie.objects.filter(
+            watched=False
+        ).order_by('-vote_count')[:30]
+    else:
+        movies = Movie.objects.filter(
+            watched=False
+        ).filter(
+            recommended=True
+        ).order_by('-vote_count')[:30]
+    context['movie_list'] = movies
+    return context
